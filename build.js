42d1658a49de3870f60a531c4247df42a7bfc0e8
bobtheintern <jpthede@gmail.com>
2016-03-27 23:15:07 -0500

    updated build.js for android

--- build.js
+++ build.js
@@ -5,3 +5,25 @@ var buildPromise = stealTools.build({
 }, {
   bundleAssets: true
 });
+// options added by `donejs add cordova` - START
+var cordovaOptions = {
+  buildDir: "./build/cordova",
+  id: "com.donejs.placemyorder",
+  name: "place my order",
+  platforms: ["android","wp8","windows"],
+  plugins: ["cordova-plugin-transport-security"],
+  index: __dirname + "/production.html",
+  glob: [
+    "node_modules/steal/steal.production.js"
+    "node_modules/place-my-order-assets/images/**/*"
+  ]
+};
+
+var stealCordova = require("steal-cordova")(cordovaOptions);
+// Check if the cordova option is passed.
+var buildCordova = process.argv.indexOf("cordova") > 0;
+
+if(buildCordova) {
+  buildPromise.then(stealCordova.build).then(stealCordova.android.emulate);
+}
+// options added by `donejs add cordova` - END
